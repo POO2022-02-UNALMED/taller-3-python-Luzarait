ad5031c5653518d0c958e3dc1028686753d4ed97
Luzarait <71353401+Luzarait@users.noreply.github.com>
2022-08-29 23:24:20 -0500

    python

---
+++ televisores/marca.py
@@ -0,0 +1,10 @@
+#HO
+class Marca:
+    def __init__(self, nombre):
+        self._nombre = nombre
+
+    def getNombre(self):
+        return self._nombre
+
+    def setNombre(self, nombre):
+        self._nombre=nombre
